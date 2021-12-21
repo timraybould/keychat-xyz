293dd3f1066a094895e41a832113a46a6fbf7c7e
Tim Raybould <timraybould@me.com>
2021-12-20 22:13:51 -0500

    add .well-known for matrix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/.well-known/matrix/server',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
 }
