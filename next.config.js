87cdb497307736b891f62d0d84838f1241f36b63
Tim Raybould <timraybould@me.com>
2021-12-22 20:48:16 -0500

    siwe

--- next.config.js
+++ next.config.js
@@ -13,4 +13,10 @@ module.exports = {
       },
     ];
   },
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false, os: false, http: false, https: false, stream: false };
+
+    return config;
+  },
 }
